dd223c9a747c1199d9b84494c6e3124ebe014497
Kiran Kulangara <kolam@Kirans-MacBook-Air.local>
2021-12-08 19:14:59 +0200

    submissions for part2 - Course information project

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+    return (
+      <h2>{course.name}</h2>
+    )
+  }
+  
+  const Total = ({ course }) => {
+    const sum = course.parts.reduce((s, p) => s + p.exercises, 0)
+    return(
+      <h3>total of {sum} exercises</h3>
+    ) 
+  }
+  
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>    
+    )
+  }
+  
+  const Content = ({ course }) => {
+    return (
+      <div>
+        {course.parts.map(part => 
+          <Part key={part.id} part={part} />
+        )}
+      </div>
+    )
+  }
+  
+  const Course = ({course}) => {
+  
+    return (
+      <div>
+        <Header course={course} />
+        <Content course={course} />
+        <Total course={course} />
+      </div>
+    )
+  }
+
+  export default Course
